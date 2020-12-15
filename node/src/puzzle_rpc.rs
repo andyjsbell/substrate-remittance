23e9eeade79934ef9dbbcd8f993f5f9e592a02c4
Andy Bell <andy.bell@barkingmad.io>
2020-12-15 15:56:44 +0100

    return hex

--- node/src/puzzle_rpc.rs
+++ node/src/puzzle_rpc.rs
@@ -1,25 +1,29 @@
-use jsonrpc_core::Result;
+use jsonrpc_core::{Result, Error};
 use jsonrpc_derive::rpc;
-use sp_io::hashing::blake2_256;
-//use codec::{Encode, Decode};
+use sp_core::blake2_256;
 
 type Hash = [u8; 32];
 
 #[rpc]
 pub trait PuzzleRpc {
     #[rpc(name = "puzzle")]
-    fn create_puzzle(&self, recipient: String, password: String) -> Result<Hash>;
+    fn create_puzzle(&self, recipient: String, password: String) -> Result<String>;
 }
 
 pub struct Puzzle;
 
 impl PuzzleRpc for Puzzle
 {
-    fn create_puzzle(&self, recipient: String, password: String) -> Result<Hash> {
+    fn create_puzzle(&self, recipient: String, password: String) -> Result<String> {
 
+        if recipient == "" || password == "" {
+            return Err(Error::new(jsonrpc_core::types::error::ErrorCode::InvalidRequest))
+        }
         let mut data = Vec::from(recipient);
         data.append(&mut Vec::from(password));
 
-        Ok(blake2_256(&data))
+        let hash = blake2_256(&data);
+
+        Ok(hex::encode(hash))
     }
 }
